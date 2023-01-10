f77400b8572dd71b4625d4e5efc7113b9de03d22
Kiran M. S <71029691+Stark0204@users.noreply.github.com>
2023-01-10 03:05:15 +0100

    Update lightning.py

--- openpack_torch/lightning.py
+++ openpack_torch/lightning.py
@@ -39,7 +39,7 @@ class BaseLightningModule(pl.LightningModule):
             )
             if self.cfg.train.optimizer.scheduler.type == "Cosine":
                 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, step_size=self.cfg.train.optimizer.scheduler.steps, gamma=0.1)
-                return optimizer, scheduler
+                return {"optimizer": optimizer, "lr_scheduler": scheduler}
         else:
             raise ValueError(
                 f"{self.cfg.train.optimizer.type} is not supported.")
